8b01cbcde79203b1d62fcf6de339c191ffd28fed
ArHiFranki <franki550@gmail.com>
2022-05-19 18:15:26 +0700

    Добавил радиус преследования для противника

--- Assets/Scripts/Emeny/EnemyAI.cs
+++ Assets/Scripts/Emeny/EnemyAI.cs
@@ -3,19 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] private Transform target;
+    [SerializeField] private float chaseRange = 5f;
 
     private NavMeshAgent navMeshAgent;
+    private float distanceToTarget = Mathf.Infinity;
 
-    private void Start()
+    private void Awake()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     private void Update()
     {
-        navMeshAgent.SetDestination(target.transform.position);
+        ChasingPlayer();
+    }
+
+    private void ChasingPlayer()
+    {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+
+        if (distanceToTarget <= chaseRange)
+        {
+            navMeshAgent.SetDestination(target.transform.position);
+        }
     }
 }
