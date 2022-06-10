48878272fdc1dbfe391f5e84b796693027a62ec9
ArHiFranki <franki550@gmail.com>
2022-06-10 17:21:02 +0700

    Доработал поведение противников

--- Assets/Scripts/Emeny/EnemyAI.cs
+++ Assets/Scripts/Emeny/EnemyAI.cs
@@ -11,6 +11,7 @@ public class EnemyAI : MonoBehaviour
 
     private NavMeshAgent navMeshAgent;
     private float distanceToTarget = Mathf.Infinity;
+    private bool isProvoked = false;
 
     private void Awake()
     {
@@ -19,17 +20,44 @@ public class EnemyAI : MonoBehaviour
 
     private void Update()
     {
-        ChasingPlayer();
+        EnemyBehavior();
     }
 
-    private void ChasingPlayer()
+    private void EnemyBehavior()
     {
         distanceToTarget = Vector3.Distance(target.position, transform.position);
 
-        if (distanceToTarget <= chaseRange)
+        if (isProvoked)
         {
-            navMeshAgent.SetDestination(target.transform.position);
+            EngageTarget();
         }
+        else if (distanceToTarget <= chaseRange)
+        {
+            isProvoked = true;
+        }
+    }
+
+    private void EngageTarget()
+    {
+        if (distanceToTarget > navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+
+        if (distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackTarget();
+        }
+    }
+
+    private void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.transform.position);
+    }
+
+    private void AttackTarget()
+    {
+        Debug.Log(name + " has attack " + target.name);
     }
 
     private void OnDrawGizmosSelected()
