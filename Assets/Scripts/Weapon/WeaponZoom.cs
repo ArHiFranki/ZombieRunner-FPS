c4b14999c41e9474155e0ecd74ccc91999c0a247
ArHiFranki <franki550@gmail.com>
2022-06-26 14:40:14 +0700

    Добавил изменение чувствительности мышки при зуммировании

--- Assets/Scripts/Weapon/WeaponZoom.cs
+++ Assets/Scripts/Weapon/WeaponZoom.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
+[RequireComponent(typeof(RigidbodyFirstPersonController))]
 public class WeaponZoom : MonoBehaviour
 {
     [SerializeField] private Camera myCamera;
     [SerializeField] private float zoomInFOV = 25f;
-    
+    [SerializeField] private float zoomInSensitivity = 0.5f;
+
     private float zoomOutFOV;
+    private float zoomOutSensitivity;
     private bool isZoomIn = false;
+    private RigidbodyFirstPersonController myFPSController;
+
+    private void Awake()
+    {
+        myFPSController = GetComponent<RigidbodyFirstPersonController>();
+    }
 
     private void Start()
     {
         zoomOutFOV = myCamera.fieldOfView;
+        zoomOutSensitivity = myFPSController.mouseLook.XSensitivity;
     }
 
     private void Update()
@@ -27,10 +38,14 @@ public class WeaponZoom : MonoBehaviour
             if (isZoomIn)
             {
                 myCamera.fieldOfView = zoomOutFOV;
+                myFPSController.mouseLook.XSensitivity = zoomOutSensitivity;
+                myFPSController.mouseLook.YSensitivity = zoomOutSensitivity;
             }
             else
             {
                 myCamera.fieldOfView = zoomInFOV;
+                myFPSController.mouseLook.XSensitivity = zoomInSensitivity;
+                myFPSController.mouseLook.YSensitivity = zoomInSensitivity;
             }
 
             isZoomIn = !isZoomIn;
