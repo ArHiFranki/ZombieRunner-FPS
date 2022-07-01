9979b00316b05d59b1ccd761bb3269b894f60cc9
ArHiFranki <franki550@gmail.com>
2022-07-01 11:13:33 +0700

    Изменил направление прокрутки колеса при смене оружия

--- Assets/Scripts/Weapon/WeaponSwitcher.cs
+++ Assets/Scripts/Weapon/WeaponSwitcher.cs
@@ -64,7 +64,7 @@ public class WeaponSwitcher : MonoBehaviour
 
     private void ProcessScrollWheel()
     {
-        if (Input.GetAxis("Mouse ScrollWheel") > 0)
+        if (Input.GetAxis("Mouse ScrollWheel") < 0)
         {
             if (currentWeapon >= transform.childCount - 1)
             {
@@ -76,7 +76,7 @@ public class WeaponSwitcher : MonoBehaviour
             }
         }
 
-        if (Input.GetAxis("Mouse ScrollWheel") < 0)
+        if (Input.GetAxis("Mouse ScrollWheel") > 0)
         {
             if (currentWeapon <= 0)
             {
